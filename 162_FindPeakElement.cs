2ce744dd448cfb09f5d736676951faa93abf9f5e
z7016187 <asd880810@gmail.com>
2023-10-12 21:23:50 +0800

    find peak element

---
+++ 162_FindPeakElement.cs
@@ -0,0 +1,34 @@
+public class Solution
+{
+    public int FindPeakElement(int[] nums)
+    {
+        int[] input = nums;
+        if (input.Length <= 1) return 0;
+        int lp = 0;
+        int rp = input.Length - 1;
+        int md = getMid(lp, rp);
+        while (lp <= rp)
+        {
+            md = getMid(lp, rp);
+            if ((md == 0 || input[md] > input[md - 1]) && (md == input.Length - 1 || input[md] > input[md + 1]))
+            {
+                return md;
+            }
+            else if (input[md] > input[md + 1])
+            {
+                rp = md - 1;
+
+            }
+            else
+            {
+                lp = md + 1;
+            }
+        }
+        return md;
+    }
+
+    public int getMid(int left, int right)
+    {
+        return (left + right) / 2;
+    }
+}

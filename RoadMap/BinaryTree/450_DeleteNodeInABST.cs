32e15873443308affcf0380e0aa111b70cd4aa98
bnncp <asd880810@gmail.com>
2024-05-03 23:55:12 +0800

    20240503 practice

---
+++ RoadMap/BinaryTree/450_DeleteNodeInABST.cs
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode DeleteNode(TreeNode root, int key)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+        if (key > root.val)
+        {
+            root.right = DeleteNode(root.right, key);
+        }
+        else if (key < root.val)
+        {
+            root.left = DeleteNode(root.left, key);
+        }
+        else
+        {
+            if (root.left == null)
+            {
+                return root.right;
+            }
+            else if (root.right == null)
+            {
+                return root.left;
+            }
+            else
+            {
+                TreeNode minNode = minValueNode(root.right);
+                root.val = minNode.val;
+                root.right = DeleteNode(root.right, minNode.val);
+            }
+        }
+        return root;
+    }
+
+    private TreeNode minValueNode(TreeNode root)
+    {
+        while (root != null && root.left != null)
+        {
+            root = root.left;
+        }
+        return root;
+    }
+}
--- RoadMap/BinaryTree/701_InsertIntoABinarySearchTree.cs
+++ RoadMap/BinaryTree/701_InsertIntoABinarySearchTree.cs
@@ -15,6 +15,19 @@ public class Solution
 {
     public TreeNode InsertIntoBST(TreeNode root, int val)
     {
+        if (root == null)
+        {
+            return new TreeNode(val);
+        }
 
+        if (val > root.val)
+        {
+            root.right = InsertIntoBST(root.right, val);
+        }
+        else if (val < root.val)
+        {
+            root.left = InsertIntoBST(root.left, val);
+        }
+        return root;
     }
 }
